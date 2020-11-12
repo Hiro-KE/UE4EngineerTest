a0bf2c1861f50732b09881a778cf6d179a8fc49a
hirodemon <jedfakhfekh@gmail.com>
2020-11-12 17:57:16 +0100

    Added proper locomotion for the Robot.

--- Source/AIReverie_Challenge/Private/Core/AIReverie_Robot.cpp
+++ Source/AIReverie_Challenge/Private/Core/AIReverie_Robot.cpp
@@ -2,7 +2,9 @@
 
 
 #include "Core/AIReverie_Robot.h"
+#include "AIReverie_Challenge/AIReverie_Challenge.h"
 #include "DrawDebugHelpers.h"
+#include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
@@ -10,14 +12,18 @@ AAIReverie_Robot::AAIReverie_Robot()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	bGroundCheck = false;
+	bDisplayDebug = false;
 }
 
 // Called when the game starts or when spawned
 void AAIReverie_Robot::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	LastLocation = GetActorLocation();
+
+	MotionDelegate.BindUFunction(this, FName("ProcessMotion"), bDisplayDebug);
+	GetWorld()->GetTimerManager().SetTimer(MotionTimer, MotionDelegate, TimerRate, true);
 }
 
 // Called every frame
@@ -34,42 +40,88 @@ void AAIReverie_Robot::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 
 }
 
-void AAIReverie_Robot::MoveRobot(const float Units /*= 10.f*/)
+void AAIReverie_Robot::MoveRobot(const float Units /*= 10.f*/, const bool bDebug /*= false*/)
 {
-	  SetActorLocation(GetActorForwardVector() * Units + GetActorLocation());
-}
+	FVector TargetLocation = GetActorForwardVector() * Units + GetActorLocation();
 
-void AAIReverie_Robot::RotateRobot(const TEnumAsByte<EDirection> Direction, const float Degree)
-{
-	UKismetMathLibrary::MapRangeClamped(UKismetMathLibrary::RandomFloat(), 0.f, 1.f, 5.f, 25.f);
-}
-
-TEnumAsByte<EDirection> AAIReverie_Robot::PickBestDirection(const float Degree, const float MaxRange /*= 25.f*/) const
-{
-	//getworlRo
-	return EDirection::Right;
-}
-
-float AAIReverie_Robot::CheckDistance(const float MaxRange /*= 50.f*/, const bool bDebug /*= false*/) const 
-{
-	if (UWorld * World = GetWorld()) {
-		FHitResult OutHit;
-		FVector LastPoint = GetActorForwardVector() * MaxRange + GetActorLocation();
-		FCollisionQueryParams CollisionParams;
-		CollisionParams.AddIgnoredActor(this);
-		if (World->LineTraceSingleByChannel(OutHit, GetActorLocation(), LastPoint, ECC_WorldStatic, CollisionParams))
-		{
-			if (bDebug) DrawDebugLine(GetWorld(), GetActorLocation(), LastPoint, FColor::Green, false, 1, 0, 1);
-			if (OutHit.bBlockingHit)
-			{
-				if (GEngine) {
-
-					GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("You are hitting: %s"), *OutHit.GetActor()->GetName()));
-					GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("Impact Point: %s"), *OutHit.ImpactPoint.ToString()));
-					return OutHit.Distance;
+	if (bGroundCheck) {
+		if (UWorld* World = GetWorld()) {
+			FHitResult OutHit;
+			FCollisionQueryParams CollisionParams;
+			CollisionParams.AddIgnoredActor(this);
+			if (World->LineTraceSingleByChannel(OutHit, TargetLocation, TargetLocation + (GetActorUpVector() * (-500.f)), ECC_Visibility, CollisionParams)) {
+				if (bDebug) DrawDebugLine(GetWorld(), TargetLocation, TargetLocation + (GetActorUpVector() * (-500.f)), FColor::Green, false, 1, 0, 1);
+				if (OutHit.bBlockingHit && OutHit.Component->ComponentHasTag("Ground")) {
+					TargetLocation = OutHit.ImpactPoint + UnitsAboveGround;
 				}
 			}
 		}
 	}
-	return 0.f;
+
+	SetActorLocation(TargetLocation, true);
+}
+
+void AAIReverie_Robot::RotateRobot(const TEnumAsByte<EDirection> Direction, const float Degree)
+{
+	if (Direction == EDirection::Right) SetActorRotation(FRotator(GetActorRotation().Pitch, GetActorRotation().Yaw + Degree, GetActorRotation().Roll));
+	else SetActorRotation(FRotator(GetActorRotation().Pitch, GetActorRotation().Yaw - Degree, GetActorRotation().Roll));
+}
+
+TEnumAsByte<EDirection> AAIReverie_Robot::PickBestDirection(const float Degree, const float MaxRange /*= 25.f*/, const bool bDebug /*= false*/) const
+{
+	FVector RotationVector_Right = UKismetMathLibrary::GreaterGreater_VectorRotator(GetActorForwardVector(), FRotator(0.f, Degree, 0.f));
+	FVector RotationVector_Left = UKismetMathLibrary::GreaterGreater_VectorRotator(GetActorForwardVector(), FRotator(0.f, -Degree, 0.f));
+
+	float DistanceRight = GetTracedDistance(RotationVector_Right, MaxRange, bDebug);
+	float DistanceLeft = GetTracedDistance(RotationVector_Left, MaxRange, bDebug);
+	if (bDebug) UE_LOG(LogAIReverie_Challenge, Log, TEXT("Robot is going to turn, Furthest point right : %f, Furthest point left : %f"), DistanceRight, DistanceLeft);
+
+	if (DistanceRight == -1.f) return EDirection::Right;
+	else if (DistanceLeft == -1.f) return EDirection::Left;
+	else if (DistanceRight >= DistanceLeft) return EDirection::Right;
+	return EDirection::Left;
+}
+
+float AAIReverie_Robot::GetTracedDistance(const FVector RotationVector, const float MaxRange /*= 50.f*/, const bool bDebug /*= false*/) const
+{
+	if (UWorld * World = GetWorld()) {
+		FHitResult OutHit;
+		FVector LastPoint = RotationVector * MaxRange + GetActorLocation();
+		FCollisionQueryParams CollisionParams;
+		CollisionParams.AddIgnoredActor(this);
+		if (World->LineTraceSingleByChannel(OutHit, GetActorLocation(), LastPoint, ECC_Visibility, CollisionParams)) {
+			if (bDebug) DrawDebugLine(GetWorld(), GetActorLocation(), LastPoint, FColor::Green, false, 1, 0, 1);
+			if (OutHit.bBlockingHit) return OutHit.Distance;
+		}
+	}
+	return -1.f;
+}
+
+
+
+void AAIReverie_Robot::ProcessMotion(const bool bDebug /*= false*/)
+{
+	// Move the robot if there is nothing in front of it
+	float ForwardDistance = GetTracedDistance(GetActorForwardVector(), HitDistance + 1, bDebug);
+	//UE_LOG(LogAIReverie_Challenge, Log, TEXT("Forward distance to the next actor is : %f"), ForwardDistance);
+	if (ForwardDistance > HitDistance || ForwardDistance == -1.f) {
+		MoveRobot(UnitsToMove, bDebug);
+	}
+	else {
+		// Pick a random Degree between 5 and 25
+		float Degree = UKismetMathLibrary::MapRangeClamped(UKismetMathLibrary::RandomFloat(), 0.f, 1.f, 5.f, 25.f);
+		// Check if the robot has moved at least twice since the last check
+		if (UKismetMathLibrary::Vector_Distance(GetActorLocation(), LastLocation) > (UnitsToMove * 3)) {
+			// Picks the best direction depending on the distance to the upcoming object from both sides
+			TEnumAsByte <EDirection> Direction = PickBestDirection(Degree, HitDistance * 2, bDebug);
+			// Rotate the robot to the Direction and degree
+			RotateRobot(Direction, Degree);
+			LastLocation = GetActorLocation();
+			LastDirection = Direction;
+		}
+		else {
+			RotateRobot(LastDirection, Degree);
+			LastLocation = GetActorLocation();
+		}
+	}
 }
--- Source/AIReverie_Challenge/Public/Core/AIReverie_Robot.h
+++ Source/AIReverie_Challenge/Public/Core/AIReverie_Robot.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "TimerManager.h"
 #include "AIReverie_Robot.generated.h"
 
 UENUM(BlueprintType)
@@ -19,17 +20,36 @@ class AIREVERIE_CHALLENGE_API AAIReverie_Robot : public APawn
 
 public:
 
-	/** Units above the floor represent the eye height of the robot (usually 10).*/
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "AIReverie|Data")
-	float UnitsAboveFloor = 10.f;
+	/** Hit Distance value represents the minimum distance in between the robot and the object in front of it. The robot will rotate to another direction once it reaches the HitDistance threshold.*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "AIReverie|Motion")
+	float HitDistance = 15.f;
+	/** Units the robot can move when moving forward.*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "AIReverie|Motion")
+	float UnitsToMove = 10.f;
+	/** Motion and capture timer rate in seconds (Ticking rate delay).*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "AIReverie|Motion")
+	float TimerRate = 1.f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "AIReverie|Motion")
+	uint8 bDisplayDebug : 1 ;
+
+	/** Units above the floor that represent the position of the robot comparing to the floor.*/
+	float UnitsAboveGround = 10.f;
 	/** Ticking this will make sure that the robot will always be on the surface of the ground by the units above the floor. Useful when the ground is not planar.*/
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "AIReverie|Data")
 	uint8 bGroundCheck : 1;
+
+protected:
+
+	UPROPERTY()
+	FVector LastLocation;
+	UPROPERTY()
+	TEnumAsByte<EDirection> LastDirection;
+	UPROPERTY()
+	FTimerHandle MotionTimer;
+	FTimerDelegate MotionDelegate;
 
 public:
 	// Sets default values for this pawn's properties
 	AAIReverie_Robot();
-
 
 protected:
 	// Called when the game starts or when spawned
@@ -43,7 +63,7 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	UFUNCTION(BlueprintCallable, Category = "AIReverie|Motion")
-	void MoveRobot(const float Units = 10.f);
+	void MoveRobot(const float Units = 10.f, const bool bDebug = false);
 
 	UFUNCTION(BlueprintCallable, Category = "AIReverie|Motion")
 	void RotateRobot(const TEnumAsByte<EDirection> Direction, const float Degree);
@@ -55,8 +75,17 @@ public:
 	 * @return Returns which direction is the best to choose (Returns right in case both are higher than the given MaxRange).
 	 */
 	UFUNCTION(BlueprintCallable, Category = "AiReverie|Motion")
-	TEnumAsByte<EDirection> PickBestDirection(const float Degree, const float MaxRange = 25.f) const;
-
+	TEnumAsByte<EDirection> PickBestDirection(const float Degree, const float MaxRange = 25.f, const bool bDebug = false) const;
+	UFUNCTION(BlueprintCallable, Category = "AiReverie|Motion")
+	void ProcessMotion(const bool bDebug = false);
+	/**
+	 * Returns the distance to the closest actor in range with a given rotation to look at from the current (Robot) actor. 
+	 * In case there is no actor in range, returns -1 as distance.
+	 * Note : Tracing channel is visibility.
+	 */
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "AiReverie|Utility")
-	float CheckDistance(const float MaxRange = 50.f, const bool bDebug = false) const;
+	float GetTracedDistance(const FVector RotationVector, const float MaxRange = 50.f, const bool bDebug = false) const;
+	//UFUNCTION(BlueprintCallable, BlueprintPure, Category = "AiReverie|Utility")
+	//void GetRenderedActors(TArray<AActor*>& CurrentlyRenderedActors, const float MinRecentTime = 0.01f);
+
 };
